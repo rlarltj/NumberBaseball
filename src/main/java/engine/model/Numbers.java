88284f0baeb03ccc8410861391b1b7d2c39fb2f1
김기서 <5rne@naver.com>
2022-10-21 21:05:12 +0900

    난이도, 제한 횟수 기능 추가

--- src/main/java/engine/model/Numbers.java
+++ src/main/java/engine/model/Numbers.java
@@ -1,16 +1,34 @@
 package engine.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.util.function.BiConsumer;
 
-@AllArgsConstructor
+@Getter
 public class Numbers {
     private Integer[] nums;
+    private StringBuilder sb;
+
+    public Numbers(Integer[] nums) {
+        this.nums = nums;
+    }
 
     public void indexedForEach(BiConsumer<Integer, Integer> consumer) {
         for (int i = 0; i < nums.length; i++) {
             consumer.accept(nums[i], i);
         }
+    }
+
+    public StringBuilder printAnswer() {
+        sb = new StringBuilder();
+        sb.append("정답은 [ ");
+        for (int i = 0; i < nums.length; i++) {
+            sb.append(nums[i]+" ");
+        }
+        sb.append("]입니다.");
+
+        return sb;
     }
 }
